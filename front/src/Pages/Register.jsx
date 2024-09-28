aa92a3b99b4d6b16a1578ad71c6358e65c924963
AdequacyOFF <adequacyoff-AO@yandex.ru>
2024-09-28 23:50:05 +0300

    Переделал регистрацию на вход. Добавил fetch с отправкой логи/пароль и получением id

--- front/src/Pages/Register.jsx
+++ front/src/Pages/Register.jsx
@@ -1,26 +1,63 @@
-import '../Pages_css/Register.css';
-import React from "react";
-import {Link} from "react-router-dom"
-
+import "../Pages_css/Register.css";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Register() {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [userId, setUserId] = useState(null);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:8080/api/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ login, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.id) {
+          console.log("User ID:", data.id);
+          setUserId(data.id);
+        } else {
+          console.error("Login failed:", data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   return (
     <div className="section">
-      <form className='answer-txt'>
-        <h1>РЕГИСТРАЦИЯ</h1>
+      <form className="answer-txt" onSubmit={handleLogin}>
+        <h1>ВХОД</h1>
         <div className="registrationField">
-          <input type="text" placeholder="Фамилия" />
-          <input type="text" placeholder="Имя" />
-          <input type="text" placeholder="Отчество" />
-          <input type="text" placeholder="Почта" />
-          <input type="password" placeholder="Пароль" minlength="8" required/>
-          <input type="password" placeholder="Повторить пароль" minlength="8" required/>
+          <input
+            type="text"
+            placeholder="Логин"
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Пароль"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            minLength="8"
+            required
+          />
         </div>
-        <Link to='/Home'><button className='registerBtn'>ЗАРЕГЕСТРИРОВАТЬСЯ</button></Link>
+        <button className="registerBtn" type="submit">
+          ВОЙТИ
+        </button>
       </form>
+      {userId && <Link to="/Home">Перейти на домашнюю страницу</Link>}
     </div>
   );
-};
+}
 
 export default Register;
